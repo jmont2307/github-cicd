aa2c78dfe7927b0da8c6e20b44b15e29783e251d
jmont2307 <jh.montalvoiv@gmail.com>
2025-04-16 23:47:33 -0600

    Fix build issues: disable exactOptionalPropertyTypes and fix JSON import

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,7 +1,14 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
-import pythonQuestions from './pythonQuestions.json';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pythonQuestionsPath = path.join(__dirname, 'pythonQuestions.json');
+const pythonQuestions = JSON.parse(fs.readFileSync(pythonQuestionsPath, 'utf8'));
 
 db.once('open', async () => {
   await cleanDB('Question', 'questions');
