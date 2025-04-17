22bbcc2bd256b62b27f7be41f126849f3e5cb81f
jmont2307 <jh.montalvoiv@gmail.com>
2025-04-16 23:34:28 -0600

    fix

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,17 +1,7 @@
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
-
-import { readFile } from 'fs/promises';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const pythonQuestionsPath = join(__dirname, './pythonQuestions.json');
-const pythonQuestionsData = await readFile(pythonQuestionsPath, 'utf8');
-const pythonQuestions = JSON.parse(pythonQuestionsData);
+import pythonQuestions from './pythonQuestions.json';
 
 db.once('open', async () => {
   await cleanDB('Question', 'questions');
